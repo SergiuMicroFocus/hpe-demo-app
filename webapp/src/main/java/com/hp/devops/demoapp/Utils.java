4201775de891008bd6d18a8a1b862161a0b098ff
SergiuMicroFocus <80881041+SergiuMicroFocus@users.noreply.github.com>
2021-10-08 15:43:51 +0300

    feature #1003
    feature #1003

--- webapp/src/main/java/com/hp/devops/demoapp/Utils.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Utils.java
@@ -10,7 +10,7 @@ public class Utils {
 		if (input != null) {
 			for (String node : input.split("/")) {
 				if (node.compareTo("") != 0 && node.compareTo("api") != 0) {
-					System.out.println("updated to fail test sdf  ");
+					System.out.println("updated to fail test sdff  ");
 					nodes.add(node);
 					System.out.println("updated to fail test sdf  ");
 					System.out.println("updated to fail test sdf  ");
